3ebb195625075a374827f4fb7702bde7eff27438
lindaluisa <lindaluisarosenthal@gmail.com>
2018-02-10 20:26:09 +0100

    Adding user model and mongoose connection

--- troublemaker/app.js
+++ troublemaker/app.js
@@ -5,23 +5,27 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
 
 const app = express();
 
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/troublemaker', {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE
+});
+
 // view engine setup
-
-// --
-
 app.use(expressLayouts);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('layout', '_layout');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
