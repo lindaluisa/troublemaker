28908f7bdc17bf1d83a2d222f4e528ce9ce7970e
lindaluisa <lindaluisarosenthal@gmail.com>
2018-02-10 21:41:15 +0100

    Post signup + creating first user

--- troublemaker/routes/auth.js
+++ troublemaker/routes/auth.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/user');
+
 /* GET users listing. */
 // sign-up route
 router.get('/signup', (req, res, next) => {
   res.render('signup');
+});
+
+router.post('/signup', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const newUser = new User({
+    username: username,
+    password: password
+  });
+  newUser.save((err) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.redirect('revolutions');
+    }
+  });
 });
 
 module.exports = router;
