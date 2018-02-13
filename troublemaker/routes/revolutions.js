2be6334eafbdaa6b4b7860e338b2ae728b642da1
JoniWanKenobi <joniwankenoby@gmail.com>
2018-02-13 15:00:09 +0100

    Join and leave functionalities

--- troublemaker/routes/revolutions.js
+++ troublemaker/routes/revolutions.js
@@ -3,12 +3,79 @@ const router = express.Router();
 
 const Revolution = require('../models/revolutions');
 
+function checkIfParticipates (participantsArr, userId) {
+  let isParticipant = false;
+  if (participantsArr.length > 0) {
+    if (participantsArr.indexOf(userId) !== -1) {
+      isParticipant = true;
+    }
+  }
+  return isParticipant;
+};
+
 router.use((req, res, next) => {
   if (req.session.currentUser) {
     next();
   } else {
     res.redirect('/login');
   }
+});
+
+router.get('/revolution-details/:revolutionId', (req, res, next) => {
+  const revolutionId = req.params.revolutionId;
+  Revolution.findOne({'_id': revolutionId})
+    .populate('creator')
+    .then((result) => {
+      const data = {
+        revolution: result,
+        isParticipant: checkIfParticipates(result.participants, req.session.currentUser._id)
+      };
+      console.log('revolution.participants: ', result.participants);
+      return data;
+    })
+    .then((data) => {
+      res.render('revolution-details', data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/leave/:revolutionId', (req, res, next) => {
+  const revolutionId = req.params.revolutionId;
+  Revolution.findOne({'_id': revolutionId})
+    .then((revolution) => {
+      const isParticipant = checkIfParticipates(revolution.participants, req.session.currentUser._id);
+      const index = revolution.participants.indexOf(req.session.currentUser._id);
+      const removed = revolution.participants.splice(index, 1);
+      const data = {
+        revolution: revolution,
+        isParticipant: checkIfParticipates(revolution.participants, req.session.currentUser._id)
+      };
+      res.render('revolution-details', data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/join/:revolutionId', (req, res, next) => {
+  const revolutionId = req.params.revolutionId;
+  const userId = req.session.currentUser._id;
+  Revolution.findOne({'_id': revolutionId})
+    .populate('creator')
+    .then((revolution) => {
+      const isParticipant = checkIfParticipates(revolution.participants, userId);
+      if (!isParticipant) {
+        revolution.participants.push(userId);
+      }
+      const data = {
+        revolution: revolution,
+        isParticipant: checkIfParticipates(revolution.participants, userId)
+      };
+      console.log('revolution.creator: ', revolution.creator);
+      res.render('revolution-details', data);
+    });
 });
 
 /* GET create-rev */
@@ -73,10 +140,6 @@ router.get('/revolutions', (req, res, next) => {
     .then((revolutions) => {
       res.render('revolutions', {revolutions});
     });
-});
-
-router.get('/revolution-details', (req, res, next) => {
-  res.render('revolution-details');
 });
 
 module.exports = router;
